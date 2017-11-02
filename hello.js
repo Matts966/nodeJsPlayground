e188086d19368f9bcc8bbcc37ff1097db485d418
Matts966 <mattsmasa96604a@gmail.com>
2017-11-02 11:48:40 +0900

    hello nodeJs

--- hello.js
+++ hello.js
@@ -3,8 +3,8 @@ var http = require('http');
 var server = http.createServer(
     function (request, response) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write('Hello World!!');
+        response.write('Hello, World!');
         response.end();
     }
-).listen(3000);
-util.log('Server running at http://localhost:3000/');
+).listen(8080);
+util.log('Server running at http://localhost:8080/');
